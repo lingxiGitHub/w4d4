a43c60ef3ecfdd99b32b53585058f6ef39af11db
lingxiGitHub <lingxi2015fall@gmail.com>
2022-10-20 13:28:24 -0700

    reverse-string

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,8 @@
 module.exports = function reverseString(string) {
   // Your code here
+  if (typeof string !== "string") {
+    throw new TypeError(`reverseString requires a string, not a ${typeof string}`)
+  }
+  return [...string].reverse().join("")
+
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,22 @@
 // Your code here
+const chai = require('chai')
+const expect = chai.expect
+const reverseString = require("../problems/reverse-string")
+
+describe("reverseString", () => {
+    it("should reverse a string", () => {
+        const string1 = reverseString("fun");
+
+        const expectedstring = "nuf"
+
+        expect(string1).to.equal(expectedstring);
+
+
+    });
+
+    it("should throw an error when passed a non-string value", () => {
+
+        expect(() => reverseString(10)).to.throw(TypeError);
+    })
+
+});
