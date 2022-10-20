3cd4fda9f845f33cae1d9648ef763084e86522bc
lilly <lcrhidalgo99@gmail.com>
2022-10-20 16:46:47 -0400

    number-fun-solved

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,14 @@
 function returnsThree() {
   // Your code here
+  return 3
 }
 
 function reciprocal(n) {
   // Your code here
+  if (n < 1 || n > 1000000) {
+    throw new TypeError("Number out of range")
+  }
+  return 1 / n
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,30 @@
 // Your code here
+const chai = require('chai')
+const expect = chai.expect
+
+const {returnsThree, reciprocal } = require("../problems/number-fun")
+
+describe("returnsThree()", () => {
+    it("should return three", () => {
+        expect(returnsThree()).to.equal(3)
+    })
+})
+
+describe("reciprocal()", () => {
+    context("valid arguments", () => {
+        it ("should return the reciprocal", () => {
+            expect(reciprocal(4)).to.equal(0.25)
+            expect(reciprocal(1)).to.equal(1)
+        })
+    })
+
+    context("invalid arguments", () => {
+        it("should throw an error when a number less than 1 is passed or greater than 10000000 is passed", () => {
+            expect(() => reciprocal(0)).to.throw(TypeError)
+            expect(() => reciprocal(1000001)).to.throw(TypeError);
+        })
+
+
+    })
+
+})
