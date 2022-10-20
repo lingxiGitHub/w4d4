0c86d0cfee8e78b70412cb1e62b3b32d651c5006
lingxiGitHub <lingxi2015fall@gmail.com>
2022-10-20 16:23:34 -0700

    triangle

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,58 @@
 // Your code here
+
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    getPerimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+
+    hasValidSideLengths() {
+        return this.side1 + this.side2 > this.side3 || this.side1 + this.side3 > this.side2 || this.side2 + this.side3 > this.side1
+    }
+
+    validate() {
+        if (this.hasValidSideLengths() === true) {
+            this.isValid = true;
+        } else {
+            this.isValid = false;
+        }
+    }
+
+    static getValidTriangles(...triangles) {
+        let result = []
+        for (let triangle of triangles) {
+            if (triangle.hasValidSideLengths()) {
+                result.push(triangle)
+            }
+        }
+        return result;
+    }
+}
+
+class Scalene extends Triangle {
+    super(side1, side2, side3)
+
+    isValidTriangle = super.validate()
+
+    validate() {
+        return side1 !== side2 && side2 !== side3 && side1 !== side3
+
+    }
+
+
+}
+
+class Isosceles extends Triangle {
+    super(side1, side2, side3)
+
+    checkTwosides() {
+        return (side1 === side2 && side1 !== side3) || (side2 === side3 && side1 !== side2) || (side1 === side3 && side1 !== side2)
+    }
+}
+
+module.exports = { Triangle, Scalene, Isosceles }
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,45 @@
 // Your code here
+
+const chai = require('chai')
+const expect = chai.expect
+const { Triangle, Scalene, Isosceles } = require("../problems/triangle")
+
+describe("Triangle", () => {
+    let side1 = 3;
+    let side2 = 4;
+    let side3 = 5;
+
+
+    beforeEach(() => {
+        let triangle1 = new Triangle(side1, side2, side3)
+
+    })
+
+    describe("constructor()", () => {
+        it("should return an instance of triangle length", () => {
+            expect(triangle1).to.be.an.instanceof(Triangle)
+
+        })
+
+        it("should set side properties", () => {
+            expect(triangle1.side1).to.equal(side1);
+            expect(triangle1.side2).to.equal(side2);
+            expect(triangle1.side3).to.equal(side3)
+
+        })
+
+        describe("getPerimeter()", () => {
+            it("should return perimeter", () => {
+                expect(triangle1.getPerimeter()).to.equal(12)
+            })
+        });
+
+        describe("should return true if it is a valid triangle", () => {
+            expect(triangle1.hasValidSideLengths()).to.be.true;
+        });
+
+
+    })
+
+})
+
